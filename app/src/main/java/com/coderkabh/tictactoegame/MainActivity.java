bcf3edbfc923e6a2a1b27858cc65da648b772242
kanishk <kanishkbharadwaj333@gmail.com>
2020-07-16 18:50:35 +0530

    playerTap method's code added

--- app/src/main/java/com/coderkabh/tictactoegame/MainActivity.java
+++ app/src/main/java/com/coderkabh/tictactoegame/MainActivity.java
@@ -3,8 +3,50 @@ package com.coderkabh.tictactoegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    // Player representations
+    // 0 - X
+    // 1 - O
+
+    // State representations
+    // 0 - X
+    // 1 - O
+    // 2 - Null / No value
+
+    private int activePlayer = 0;
+    int[] gameState={2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int[][] winningPositions = { {0,1,2},
+        {3,4,5},
+        {6,7,8},
+        {0,3,6},
+        {1,4,7},
+        {2,5,8},
+        {0,4,8},
+        {2,4,6}
+    };
+
+
+
+    public void playerTap(View view){
+        ImageView img = (ImageView) view;
+        int tappedImage = Integer.parseInt(img.getTag().toString());
+        if(gameState[tappedImage] == 2){
+            gameState[tappedImage] = activePlayer;
+            img.setTranslationY(-1000f);
+            if (activePlayer == 0){
+                activePlayer = 1;
+                img.setImageResource(R.drawable.x);
+            }
+            else{
+                activePlayer = 0;
+                img.setImageResource(R.drawable.o);
+            }
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
